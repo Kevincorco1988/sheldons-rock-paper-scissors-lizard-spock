8f35d1b12c32c24c58c43f5d241f57041d286d8f
Kevincorco1988 <kevincorco1988@gmail.com>
2022-07-08 22:28:00 +0000

    Add win and lose pairs and player options

--- run.py
+++ run.py
@@ -1,5 +1,25 @@
 import random
 
+# Pairing the attack options determine who wins and loses
+winning_losing_pairs = [('Scissors', 'Paper'),
+                        ('Paper', 'Rock'),
+                        ('Rock', 'Lizard'),
+                        ('Lizard', 'Spock'),
+                        ('Spock', 'Scissors'),
+                        ('Scissors', 'Lizard'),
+                        ('Lizard', 'Paper'),
+                        ('Paper', 'Spock'),
+                        ('Spock', 'RocK'),
+                        ('Rock', 'Scissors')]
+
 attack_options = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 
-sheldon = random.choice(attack_options)
+sheldon_attack = random.choice(attack_options)
+
+# Prints the start of game options to the player
+print('Choose your weapon from the options below: ')
+print(' -> 0 for Rock\n -> 1 for Paper\n -> 2 for Scissors\n -> 3 for Lizard\n -> 4 for Spock')
+
+player_input = int(input('Enter your choice of attack (0-4): '))
+
+player_attack = attack_options[player_input]
