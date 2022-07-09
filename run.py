429cbb07b0c5177aa4421c47b6d6bd486a9b2bf3
Kevincorco1988 <kevincorco1988@gmail.com>
2022-07-09 18:10:02 +0000

    Add emoji module to file and add emojis to improve user experiencein game

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
+import emoji
 
-# Pairing the attack options determine who wins and loses
+# Pairing the attack options to determine who wins and loses
 winning_losing_pairs = [('Scissors', 'Paper'),
                         ('Paper', 'Rock'),
                         ('Rock', 'Lizard'),
@@ -17,9 +18,9 @@ attack_options = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 sheldon_attack = random.choice(attack_options)
 
 # Prints the start of game options to the player
-print('The game is Rock, Paper, Scissors, Lizard, Spock and it will be the best out of 5')
-print('Choose your weapon from the options below: ')
-print(' -> 0 for Rock\n -> 1 for Paper\n -> 2 for Scissors\n -> 3 for Lizard\n -> 4 for Spock')
+print(emoji.emojize('The game is Rock :brick:, Paper :scroll:, Scissors :scissors:, Lizard :lizard:, Spock \U0001F47D \U0001F596 and it will be the best out of 5 games 🙂'))
+print('Choose your weapon from the options below 👀: ')
+print(emoji.emojize(' -> 0 for Rock :brick:\n -> 1 for Paper :scroll:\n -> 2 for Scissors :scissors:\n -> 3 for Lizard :lizard:\n -> 4 for Spock \U0001F47D \U0001F596'))
 
 player_input = int(input('Enter your choice of attack (0-4): '))
 
@@ -27,13 +28,13 @@ player_attack = attack_options[player_input]
 
 # Gives the result of the game and a statement from Sheldon regarding the result#
 if sheldon_attack == player_attack:
-    result = 'Judge rules...Draw!!... It seems that I may have met my match'
+    result = 'Judge rules...Draw!!... It seems that I may have met my match 😏'
 
 elif (player_attack, sheldon_attack) in winning_losing_pairs:
-    result = 'Judge rules...You win that round... Lets see if you can keep it up'
+    result = 'Judge rules...You win that round... Lets see if you can keep it up 🤨'
 
 else:
-    result = 'Judge rules...I win!!....another step towards victory'
+    result = 'Judge rules...I win!!....another step towards victory 😃'
 
 print('-----------------------------------')
 print('Players attack: ', player_attack)
